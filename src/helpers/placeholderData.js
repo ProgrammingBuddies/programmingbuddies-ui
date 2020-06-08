11a14e01f85dd3b1fc5b6f2f761d2f4e69367c09
SKaralius <k.simonas.k@gmail.com>
2020-06-08 17:30:38 +0300

    Adds a helper file to generate random data for development.

---
+++ src/helpers/placeholderData.js
@@ -0,0 +1,25 @@
+import { LoremIpsum } from "lorem-ipsum";
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+});
+
+console.log(Math.ceil(Math.random() * 3));
+
+export const generateProjectList = (numberOfProjects) => {
+  const projects = [];
+  for (let i = 0; i < numberOfProjects; i++) {
+    projects.push({
+      title: lorem.generateWords(Math.ceil(Math.random() * 3)),
+      description: lorem.generateSentences(Math.ceil(Math.random() * 5)),
+    });
+  }
+  return projects;
+};
