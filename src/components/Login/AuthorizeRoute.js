deff74e322cac62f66e2a484b005c6cb456ecb48
flurach <flurach.dev@gmail.com>
2020-06-08 12:21:58 +0300

    Removed useEffect from AuthorizeRoute.js

--- src/components/Login/AuthorizeRoute.js
+++ src/components/Login/AuthorizeRoute.js
@@ -1,29 +1,15 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
 import login from "../../redux/Slices/LoginSlice";
 
-const AuthorizeRoute = ({ component: Component, ...rest }) => {
-  let isLoggedIn = useSelector((state) => state?.login?.isLoggedIn);
+const AuthorizeRoute = (props) => {
+  let isLoggedIn = useSelector((state) => state.login.isLoggedIn);
   let dispatch = useDispatch();
 
-  useEffect(() => {
-    if (!isLoggedIn) {
-      dispatch(login.actions.startLoginFlow(rest?.path));
-    }
-  }, [isLoggedIn]);
+  if (!isLoggedIn) dispatch(login.actions.startLoginFlow(props.path));
 
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        if (isLoggedIn) {
-          return <Component {...props} />;
-        }
-      }}
-    />
-  );
+  return <Route {...props} />;
 };
 
 export default AuthorizeRoute;
